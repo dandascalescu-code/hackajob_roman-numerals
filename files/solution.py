e15af8e539545011bd2349533e7141a39bc7f12e
ddascalescu <djdascalescu@gmail.com>
2023-01-05 18:49:00 +0000

    Revision

--- files/solution.py
+++ files/solution.py
@@ -4,19 +4,26 @@ class Solution:
 		#
 		# Write your code below; return type and arguments should be according to the problem\'s requirements
 		#
-		roman_ms = [1000, 500, 100, 50, 10, 5, 1]
-		roman_cs = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
-
-		roman_mprevs = [900, 400, 90, 40, 9, 4, 0]
-		roman_cprevs = ['CM', 'CD', 'XC', 'XL', 'IX', 'IV', '']
+		roman_numerals = {
+			1000: 'M',
+			900: 'CM',
+			500: 'D',
+			400: 'CD',
+			100: 'C',
+			90: 'XC',
+			50: 'L',
+			40: 'XL',
+			10: 'X',
+			9: 'IX',
+			5: 'V',
+			4: 'IV',
+			1: 'I'
+		}
 
 		n_in_roman_alphabet = ""
-		for i in range(len(roman_ms)):
-			while n >= roman_ms[i]:
-				n_in_roman_alphabet += roman_cs[i]
-				n -= roman_ms[i]
-			if n >= roman_mprevs[i] and n < roman_ms[i]:
-				n_in_roman_alphabet += roman_cprevs[i]
-				n -= roman_mprevs[i]
+		for value in roman_numerals.keys():
+			while n >= value:
+				n_in_roman_alphabet += roman_numerals[value]
+				n -= value
 		
 		return n_in_roman_alphabet
